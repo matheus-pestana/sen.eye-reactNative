12d0fb6f87c9adf6d99e4f9755230ffbd7937146
Matheus Pestana <matheus90pestana@gmail.com>
2024-05-20 01:51:59 -0300

    .

--- src/pages/Hoje.js
+++ src/pages/Hoje.js
@@ -1,143 +1,212 @@
-import React from 'react';
-import { View, Text, StyleSheet, Image, ScrollView, SafeAreaView, ImageBackground, Dimensions } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Image, ScrollView, FlatList, StyleSheet } from 'react-native';
+import { Card, Title } from 'react-native-paper';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const windowWidth = Dimensions.get('window').width;
-const windowHeight = Dimensions.get('window').height;
+export default function Hoje(props) {
+    const [info, setInfo] = useState({
+        name: "loading !!",
+        temp: "loading",
+        humidity: "loading",
+        desc: "loading",
+        icon: "loading"
+    });
 
-export default function Hoje() {
-    return (
-        <SafeAreaView style={styles.safeArea}>
-            <ImageBackground
-                source={require('../assets/background_login.jpeg')}
-                resizeMode="cover"
-                style={styles.background}>
-                <View style={styles.container}>
-                    <View style={styles.header}>
-                        <Image source={require('../assets/logo1.png')} style={styles.logo} />
-                        <View style={styles.cidade}>
-                            <Text style={styles.city}>Caçapava</Text>
-                            <Text style={styles.temperature}>25° | Nublado</Text>
-                        </View>
-                    </View>
-                    <View style={styles.weatherForecast}>
-                        <Text style={styles.forecastText}>Clima e temperatura média nos próximos dias</Text>
-                        <ScrollView horizontal contentContainerStyle={styles.forecastIcons}>
-                            <Image source={require('../assets/storm.png')} style={styles.icon} />
-                            <Image source={require('../assets/storm.png')} style={styles.icon} />
-                            <Image source={require('../assets/storm.png')} style={styles.icon} />
-                            <Image source={require('../assets/storm.png')} style={styles.icon} />
-                            <Image source={require('../assets/storm.png')} style={styles.icon} />
-                            <Image source={require('../assets/storm.png')} style={styles.icon} />
-                        </ScrollView>
-                    </View>
-                    <View style={styles.localSituation}>
-                        <Text style={styles.localSituationText}>Veja a situação de sua localidade</Text>
-                        <View style={styles.graphics}>
-                            <Text style={styles.graphicsText}>Gráficos</Text>
-                        </View>
-                    </View>
+    const [forecast, setForecast] = useState([]);
+
+    useEffect(() => {
+        getWeather();
+        getForecast();
+    }, []);
+
+    const getWeather = async () => {
+        let MyCity = await AsyncStorage.getItem("newcity");
+        if (!MyCity) {
+            if (props.route && props.route.params && props.route.params.city) {
+                MyCity = props.route.params.city;
+            } else {
+                MyCity = "Caçapava"; // Valor padrão se não houver cidade nos parâmetros ou no AsyncStorage
+            }
+        }
+
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${MyCity}&APPID=b29af4d126efcc79c4992bf80a3ecd65&units=metric&lang=pt`)
+            .then(data => data.json())
+            .then(results => {
+                setInfo({
+                    name: results.name,
+                    temp: results.main.temp,
+                    humidity: results.main.humidity,
+                    desc: results.weather[0].description,
+                    icon: results.weather[0].icon,
+                });
+            })
+            .catch(err => {
+                alert(err.message);
+            });
+    };
+
+    const getForecast = async () => {
+        let MyCity = await AsyncStorage.getItem("newcity");
+        if (!MyCity) {
+            if (props.route && props.route.params && props.route.params.city) {
+                MyCity = props.route.params.city;
+            } else {
+                MyCity = "Caçapava"; // Valor padrão se não houver cidade nos parâmetros ou no AsyncStorage
+            }
+        }
+
+        fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${MyCity}&APPID=b29af4d126efcc79c4992bf80a3ecd65&units=metric&lang=pt`)
+            .then(data => data.json())
+            .then(results => {
+                const currentDate = new Date(); // Data atual
+                let currentDayIndex = currentDate.getDay(); // Índice do dia atual (0-6, onde 0 é domingo)
+                const daysOfWeek = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'];
+
+                const forecastData = results.list
+                    .filter((entry, index) => index < 5) // Limitando a 5 dias
+                    .map(entry => {
+                        const forecastDate = new Date(entry.dt * 1000); // Data da previsão
+                        const dayIndex = (currentDayIndex + 1) % 7; // Avança para o próximo dia (considerando o ciclo semanal)
+                        currentDayIndex = dayIndex; // Atualiza o índice do dia atual para a próxima iteração
+                        return {
+                            date: daysOfWeek[dayIndex], // Obtém o nome abreviado do dia da semana
+                            temp: entry.main.temp,
+                            desc: entry.weather[0].description,
+                            icon: entry.weather[0].icon
+                        };
+                    });
+
+                setForecast(forecastData);
+            })
+            .catch(err => {
+                alert(err.message);
+            });
+    };
+
+    const renderWeatherInfo = () => (
+
+        <View style={styles.weatherInfoContainer}>
+            <Image
+                style={styles.logo}
+                source={require('../assets/logo1.png')}
+            />
+            <View style={styles.weatherDetails}>
+                <Title style={styles.cityName}>{info.name}</Title>
+                <View style={styles.temperatureContainer}>
+                    <Text style={styles.temperature}>{info.temp}° |</Text>
+                    <Text style={styles.weatherDescription}>{info.desc}</Text>
                 </View>
-            </ImageBackground>
-        </SafeAreaView>
+            </View>
+        </View>
+
+
 
     );
-};
+
+    return (
+        <ScrollView contentContainerStyle={styles.container}>
+            {renderWeatherInfo()}
+
+            <View style={styles.forecastContainer}>
+                <Text style={styles.forecastTitle}>Clima e temperatura média nos próximos dias</Text>
+                <FlatList
+                    data={forecast}
+                    keyExtractor={(item, index) => index.toString()}
+                    horizontal={true}
+                    renderItem={({ item }) => (
+                        <View style={styles.forecastItem}>
+                            <Text>{item.date}</Text>
+                            <Image style={styles.weatherIcon} source={{ uri: `https://openweathermap.org/img/w/${item.icon}.png` }} />
+                            <Text>{item.temp}°</Text>
+                        </View>
+                    )}
+                />
+            </View>
+
+            <Text style={styles.locationSituation}>Veja a situação de sua localidade</Text>
+            <Card style={styles.graphCard}>
+                <Text style={styles.graphText}>Gráficos</Text>
+            </Card>
+        </ScrollView>
+    );
+}
 
 const styles = StyleSheet.create({
-
-    safeArea: {
-        backgroundColor: '#e7efff',
-        flex: 1,
-    },
-
-    background: {
-        width: windowWidth,
-        height: '20%',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-
     container: {
-        flex: 1,
-        backgroundColor: '#E1EAF1',
-        padding: 10,
+        flexGrow: 1,
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        padding: 16,
+        backgroundColor: '#8fb2d8',
     },
 
-    header: {
+    weatherInfoContainer: {
         flexDirection: 'row',
-        gap: 60,
-        marginBottom: 20,
-        padding: 20,
-        borderRadius: 10,
+        alignItems: 'flex-start',
+        paddingHorizontal: 16,
+        paddingTop: 16,
     },
-
     logo: {
-        width: 50,
-        height: 50,
-        marginBottom: 10,
+        width: 40,
+        height: 40,
+        marginRight: 8,
     },
-
-    cidade: {
-        justifyContent: 'center',
-        alignItems: 'center',
+    weatherDetails: {
+        flexDirection: 'column',
+        alignItems: 'flex-start',
     },
-
-    city: {
+    cityName: {
         fontSize: 24,
-        fontWeight: 'bold',
+        marginBottom: 8,
     },
-
-    temperature: {
-        fontSize: 18,
-        color: '#888',
-    },
-
-    weatherForecast: {
-        marginBottom: 20,
-        backgroundColor: '#CBD8E1',
-        padding: 20,
-        borderRadius: 10,
-    },
-
-    forecastText: {
-        textAlign: 'center',
-        fontSize: 16,
-        marginBottom: 10,
-    },
-
-    forecastIcons: {
+    temperatureContainer: {
         flexDirection: 'row',
-        justifyContent: 'space-between',
-    },
-
-    icon: {
-        width: 45,
-        height: 45,
-        marginHorizontal: 5,
-    },
-
-    localSituation: {
         alignItems: 'center',
-        marginBottom: 20,
     },
-
-    localSituationText: {
+    temperature: {
+        fontSize: 20,
+        marginRight: 8,
+    },
+    weatherDescription: {
         fontSize: 16,
-        marginBottom: 10,
     },
 
-    graphics: {
+    forecastContainer: {
+        backgroundColor: '#577B93',
+        width: '100%',
+        alignItems: 'center'
+    },
+
+    forecastTitle: {
+        fontSize: 16,
+        marginVertical: 16
+    },
+
+    forecastItem: {
+        alignItems: 'center',
+        marginHorizontal: 8
+    },
+
+    weatherIcon: {
+        width: 30,
+        height: 30
+    },
+
+    locationSituation: {
+        fontSize: 18,
+        marginVertical: 16
+    },
+
+    graphCard: {
         width: '100%',
         height: 200,
-        backgroundColor: '#CBD8E1',
-        justifyContent: 'center',
+        justifyContent:
+            'center',
         alignItems: 'center',
-        borderRadius: 10,
+        backgroundColor: '#e0e0e0'
     },
 
-    graphicsText: {
-        fontSize: 24,
-        color: '#888',
-    },
-
+    graphText: {
+        fontSize: 24
+    }
 });
